830ffdce92efe44b67a51b19647bd5d5f53bb01c
Glen Holcomb <MentalLabor@gmail.com>
2016-11-11 21:29:33 -0700

    Removes old function in favor of methods on both top-level config types

--- src/config.rs
+++ src/config.rs
@@ -76,17 +76,3 @@ pub struct Crypto {
     pub pub_key_file: String,
     pub priv_key_file: String,
 }
-
-pub fn load_config() -> Option<Config> {
-    let mut config_data = String::new();
-
-    let mut config_file = match File::open("app_therapy.json") {
-        Err(why) => panic!("Couldn't open app_therapy.json: {}", why.description()),
-        Ok(file) => file,
-    };
-
-    match config_file.read_to_string(&mut config_data) {
-        Ok(_) => serde_json::from_str(&config_data).unwrap(),
-        Err(_) => None,
-    }
-}
