f1ecf20f82032866408431dad7c066b2265208ec
Glen Holcomb <MentalLabor@gmail.com>
2016-11-21 10:50:03 -0700

    do some stuff when a message is received

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,10 @@
+use std::io::Read;
 use std::net::TcpStream;
 
-pub fn process_request(stream: TcpStream) {
+pub fn process_request(mut stream: TcpStream) {
+    let mut request = vec![0; 1000];
 
+    let _ = stream.read_to_end(&mut request);
+
+    println!("{:?}", request);
 }
