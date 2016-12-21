6c7be70868331f24fc62a92fd18b689c030a9b2c
Glen Holcomb <MentalLabor@gmail.com>
2016-12-21 09:44:53 -0700

    Updates `server::process_request` to handle decryption of the message

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,12 @@
 use byteorder::{NetworkEndian, ReadBytesExt};
-use messaging::Payload;
+use crypto;
+use crypto::FileBacked;
+use messaging::{Message, Payload};
 use sodiumoxide::crypto::box_::{PublicKey, SecretKey};
 use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::sync::Arc;
 
 pub fn process_request(stream: &mut TcpStream, agent_pub: PublicKey, key_list: &Arc<HashMap<String, SecretKey>>) {
     let length = match stream.take(8).read_u64::<NetworkEndian>() {
@@ -14,15 +17,64 @@ pub fn process_request(stream: &mut TcpStream, agent_pub: PublicKey, key_list: &
         }
     };
 
-    println!("Bytes in message: {:?}", length);
-
+    // Create buffer to hold  message and fill it
     let mut message = vec![0; length as usize];
     let _ = stream.take(length).read_to_end(&mut message);
 
-    println!("Message: {:?}", message);
-    println!("Message is {} bytes long", message.len());
+    // Deconstruct the message
+    let parts = match Payload::new(&message) {
+        Some(payload) => payload,
+        None => {
+            let _ = stream.write("Failed to process the client message".as_bytes());
+            return
+        }
+    };
 
-    let parts = Payload::new(&message);
+    println!("Parts: {:?}", &parts);
 
-    println!("{:?}", parts);
+    // Find secret key for user that sent message
+    let user_key = match key_list.get(&parts.requestor) {
+        Some(key) => key.clone(),
+        None => {
+            let mut message = "No key for".to_string();
+            message.push_str(&parts.requestor);
+
+            let _ = stream.write(message.as_bytes());
+            return
+        }
+    };
+
+    println!("User key: {:?}", &user_key);
+
+    // Decrypt the box
+    let command = match crypto::un_box(&parts.the_box, &parts.nonce, &agent_pub, &user_key) {
+        Ok(plaintext) => plaintext,
+        Err(error) => {
+            println!("{:?}", error);
+            let _ = stream.write("Error: Couldn't decrypt message".as_bytes());
+            println!("Decryption error: {:?}", error);
+            return
+        }
+    };
+
+    println!("Instruction {:?}", &command);
+}
+
+//fn process_box(payload: &Payload) -> Result<Vec<u8>, Error> {
+//
+//}
+
+pub fn load_keys(keys: &Vec<Vec<String>>) -> HashMap<String, SecretKey> {
+    let mut key_map: HashMap<String, SecretKey> = HashMap::new();
+
+    for key_set in keys {
+        let key = match SecretKey::read_from(&key_set[1]) {
+            Some(secret_key) => secret_key,
+            None => continue
+        };
+
+        key_map.insert(key_set[0].clone(), key);
+    }
+
+    key_map
 }
